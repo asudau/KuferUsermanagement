929432047a65b788bcee48fdf3008990802cfd70
asudau <sudau@elan-ev.de>
2018-11-30 13:16:20 +0100

    fix #6

--- views/register/agree.php
+++ views/register/agree.php
@@ -7,22 +7,23 @@
     <tr>
         <td class="table_header_bold">
             <?= Icon::create('door-enter', 'info_alt')->asImg() ?>
-            <b><?=_("Nutzungsbedingungen")?></b>
+            <b><?=_("Nutzungsbedingungen und Datenschutzerklärung")?></b>
         </td>
     </tr>
     <tr>
         <td class="blank">
-        <p><h3 align="center">   
-	        <?= Icon::create('accept', 'clickable')->asImg()  ?>   
-                <a href="<?= URLHelper::getLink(Request::url(), array('i_accept_the_terms' => 'yes')) ?>"><b><?=_("Ich erkenne die Nutzungsbedingungen und Datenschutzerkl&auml;rung an")?></b></a>   
-            </h3></p>   
-            <br/>   
- 	</td>   
+            <div style="text-align: center">
+                <div class="button-group">
+                    <?= Studip\LinkButton::create(_('Ich erkenne die Nutzungsbedingungen an'), $controller->url_for('register/index/') . $user->id) ?>
+                    <?= Studip\LinkButton::create(_('Registrierung abbrechen'), URLHelper::getLink('index.php')) ?>
+                </div>
+            </div>
+        </td>   
     </tr>   
     <tr>   
         <td class="blank" width="50%">   
      
- 	<? //Nutzungsbedingungen und DatenschutzerklÃ?rung aus dem Impressum darstellen   
+ 	<? //Nutzungsbedingungen und Datenschutzerklärung aus dem Impressum darstellen   
  	$si = new Siteinfo();   
  	$nutzung = $si->get_detail_content_processed(8);    
  	$datenschutz = $si->get_detail_content_processed(9);    
@@ -35,8 +36,9 @@
 </table>
 
 
-<p><h3 align="center">   
-	        <?= Icon::create('accept', 'clickable')->asImg()  ?>   
-                <a href="<?= URLHelper::getLink(Request::url(), array('i_accept_the_terms' => 'yes')) ?>"><b><?=_("Ich erkenne die Nutzungsbedingungen und Datenschutzerkl&auml;rung an")?></b></a>   
-            </h3></p>   
-            <br/>   
+<div style="text-align: center">
+    <div class="button-group">
+        <?= Studip\LinkButton::create(_('Ich erkenne die Nutzungsbedingungen an'), $controller->url_for('register/index/') . $user->id) ?>
+        <?= Studip\LinkButton::create(_('Registrierung abbrechen'), URLHelper::getLink('index.php')) ?>
+    </div>
+</div> 
