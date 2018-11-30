fd5cb982d1aec89f6202901d5788a48b63dd3330
asudau <sudau@elan-ev.de>
2018-11-30 13:15:18 +0100

    rel #3

--- controllers/register.php
+++ controllers/register.php
@@ -30,22 +30,25 @@ class RegisterController extends StudipController {
     
     public function claim_account_action($user_id) //aus Seminar_Register_Auth    
     {
-        //TODO Nutzungsbedingungen etc abfragen?
         $this->error_msg = "";
-        //TODO check if account unclaimed
-
-        $this->auth["uname"] = Request::username('username'); // This provides access for "crcregister.ihtml"
+        //TODO check if account unclaimed!!!
+        //$this->auth["uname"] = Request::username('username'); // This provides access for "crcregister.ihtml"
+        
+        $username = trim(Request::get('username')); //brauchen wir für späteren vergleich
+        $Vorname = trim(Request::get('Vorname'));
+        $Nachname = trim(Request::get('Nachname'));
+        
+        $user = User::find($user_id);
+        if ($user->username != $username){
+            $this->error_msg = "Probleme mit dem Nutzernamen " . $user->username . ' ungleich ' .$username ;
+        }
 
         $validator = new email_validation_class; // Klasse zum Ueberpruefen der Eingaben
         $validator->timeout = 10; // Wie lange warten wir auf eine Antwort des Mailservers?
 
         if (!Seminar_Session::check_ticket(Request::option('login_ticket'))) {
-            return false;
+            $this->error_msg = "Ungültiges Login Ticket";
         }
-
-        $username = trim(Request::get('username'));
-        $Vorname = trim(Request::get('Vorname'));
-        $Nachname = trim(Request::get('Nachname'));
 
         // accept only registered domains if set
         $cfg = Config::GetInstance();
@@ -56,28 +59,24 @@ class RegisterController extends StudipController {
             $Email = trim(Request::get('Email'));
         }
 
-        if (!$validator->ValidateUsername($username)) {
-            $this->error_msg = $this->error_msg . _("Der gewählte Benutzername ist zu kurz!") . "<br>";
-            return false;
-        } // username syntaktisch falsch oder zu kurz
+//        if (!$validator->ValidateUsername($username)) {
+//            $this->error_msg = $this->error_msg . _("Der gewählte Benutzername ist zu kurz!") . "<br>";
+//            return false;
+//        } // username syntaktisch falsch oder zu kurz
         // auf doppelte Vergabe wird weiter unten getestet.
 
         if (!$validator->ValidatePassword(Request::quoted('password'))) {
             $this->error_msg = $this->error_msg . _("Das Passwort ist zu kurz!") . "<br>";
-            return false;
         }
 
         if (!$validator->ValidateName($Vorname)) {
             $this->error_msg = $this->error_msg . _("Der Vorname fehlt oder ist unsinnig!") . "<br>";
-            return false;
         } // Vorname nicht korrekt oder fehlend
         if (!$validator->ValidateName($Nachname)) {
             $this->error_msg = $this->error_msg . _("Der Nachname fehlt oder ist unsinnig!") . "<br>";
-            return false; // Nachname nicht korrekt oder fehlend
         }
         if (!$validator->ValidateEmailAddress($Email)) {
             $this->error_msg = $this->error_msg . _("Die E-Mail-Adresse fehlt oder ist falsch geschrieben!") . "<br>";
-            return false;
         } // E-Mail syntaktisch nicht korrekt oder fehlend
 
         $REMOTE_ADDR = $_SERVER["REMOTE_ADDR"];
@@ -85,40 +84,47 @@ class RegisterController extends StudipController {
 
         if (!$validator->ValidateEmailHost($Email)) { // Mailserver nicht erreichbar, ablehnen
             $this->error_msg = $this->error_msg . _("Der Mailserver ist nicht erreichbar, bitte überprüfen Sie, ob Sie E-Mails mit der angegebenen Adresse verschicken und empfangen können!") . "<br>";
-            return false;
         } else { // Server ereichbar
             if (!$validator->ValidateEmailBox($Email)) { // aber user unbekannt. Mail an abuse!
                 StudipMail::sendAbuseMessage("Register", "Emailbox unbekannt\n\nUser: $username\nEmail: $Email\n\nIP: $REMOTE_ADDR\nZeit: $Zeit\n");
                 $this->error_msg = $this->error_msg . _("Die angegebene E-Mail-Adresse ist nicht erreichbar, bitte überprüfen Sie Ihre Angaben!") . "<br>";
-                return false;
             } else {
                 ; // Alles paletti, jetzt kommen die Checks gegen die Datenbank...
             }
         }
-
-        // alle Checks ok, Benutzer registrieren...
-        $hasher = UserManagement::getPwdHasher();
-        $user = User::find($user_id);
-        if ($user->username != $username)
-            return false;
         
-        $user->password = $hasher->HashPassword(Request::get('password'));
-        $user->vorname = $Vorname;
-        $user->nachname = $Nachname;
-        $user->email = $Email;
-        $user->geschlecht = Request::int('geschlecht');
-  
-        $user->store();
-        if ($user->user_id) {
-            Seminar_Register_Auth::sendValidationMail($user);
-            $this->auth["perm"] = $user->perms;
-            //return $user->user_id;
-            //TODO set account claimed
-            $mapping = KuferMapping::findOneBySQL('studip_id = :user_id', [':user_id' => $user->user_id]);
-            $mapping->claimed = '1';
-            $mapping->store();
+        if($this->error_msg){
+            PageLayout::postMessage(MessageBox::error($this->error_msg));
+            $this->redirect($this->url_for('/register/index/') . $user_id);
+        } else {
+        
+            // alle Checks ok, Benutzer registrieren...
+            $hasher = UserManagement::getPwdHasher();
+
+            $user->password = $hasher->HashPassword(Request::get('password'));
+            $user->vorname = $Vorname;
+            $user->nachname = $Nachname;
+            $user->email = $Email;
+            $user->geschlecht = Request::int('geschlecht');
+
+            if ($user->store()) {
+                Seminar_Register_Auth::sendValidationMail($user);
+                $this->auth["perm"] = $user->perms;
+                //return $user->user_id;
+                //TODO set account claimed
+                $mapping = KuferMapping::findOneBySQL('studip_id = :user_id', [':user_id' => $user->user_id]);
+                if ($mapping){
+                    $mapping->claimed = '1';
+                    $mapping->store();
+                    PageLayout::postMessage(MessageBox::success(_("Ihre Registrierung wurde erfolgreich vorgenommen. ") . 
+                        _("Das System wird Ihnen zur Bestätigung eine E-Mail zusenden.")));
+                } else {
+                    //da ist aber was schief gegangen //TODO was machen wir in diesem Fall?? Kann das überhaupt passieren?
+                    PageLayout::postMessage(MessageBox::success(_("Ihre Registrierung wurde erfolgreich vorgenommen. ") . 
+                        _("Das System wird Ihnen zur Bestätigung eine E-Mail zusenden.")));
+                } 
+            }
         }
-        $this->render_nothing();
     }
 
     
